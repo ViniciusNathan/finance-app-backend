6ec37aec4ebe3eec95f19ccb3f55a5bbba6b2e2b
Vinicius Nathan <viniciusnathan@gmail.com>
2026-08-09 12:56:55 -0300

    feat: update e delete transaction backend complete

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -31,4 +31,41 @@ const getTransactionById = async (req, res) => {
   res.json(transaction);
 };
 
-export { createTransaction, listAllTransactions, getTransactionById };
+//atualizar dados da transação pelo id
+const updateTransactionById = async (req, res) => {
+  const transaction = await prisma.transaction.update({
+    //Qual registro eu devo modificar
+    where: {
+      id: Number(req.params.id),
+    },
+    //Quais valores eu devo escrever nessa linha
+    data: {
+      value: req.body.value,
+      transactionDate: req.body.transactionDate,
+      divisionType: req.body.divisionType,
+      categoryId: req.body.categoryId,
+    },
+  });
+  res.json(transaction);
+};
+
+//deletar transação pelo ID
+const deleteTransactionById = async (req, res) => {
+  const transaction = await prisma.transaction.delete({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+  res.json({
+    message: "Transação removida com sucesso",
+    transaction,
+  });
+};
+
+export {
+  createTransaction,
+  listAllTransactions,
+  getTransactionById,
+  updateTransactionById,
+  deleteTransactionById,
+};
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -2,9 +2,9 @@ import express from "express";
 import {
   listAllTransactions,
   getTransactionById,
-  //updateTransactionById,
+  updateTransactionById,
   createTransaction,
-  //deleteTransactionById,
+  deleteTransactionById,
 } from "../controllers/transaction.controller.js";
 
 const router = express.Router();
@@ -12,7 +12,7 @@ router.get("/", listAllTransactions);
 router.post("/", createTransaction);
 
 router.get("/:id", getTransactionById);
-// router.put("/:id", updateTransactionById);
-// router.delete("/:id", deleteTransactionById);
+router.put("/:id", updateTransactionById);
+router.delete("/:id", deleteTransactionById);
 
 export default router;
